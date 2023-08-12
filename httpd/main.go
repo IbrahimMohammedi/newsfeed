d57fd3e6e4c95aaefa43f42994287a56091be425
Ibrahim Mohammedi <ibrahimod77@gmail.com>
2023-08-12 19:00:43 +0100

    called the pingget function in the main function

--- httpd/main.go
+++ httpd/main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"net/http"
+	"newsfeeder/httpd/handler"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.GET("/ping", handler.PingGet)
+
+	r.Run()
+
 }
