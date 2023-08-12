94c5643aa391bad15aa11e2d77320ffe9e49abe4
Ibrahim Mohammedi <ibrahimod77@gmail.com>
2023-08-12 19:20:02 +0100

    wrote the architecture of newsfeed.go file by comments

---
+++ platform/newsfeed/newsfeed.go
@@ -0,0 +1,11 @@
+package newsfeed
+
+// Item sturcts : store the news feed items.
+
+// Repo struct : News feed list.
+
+// New func: returns a pointer to Repo.
+
+// Add func: Adds an item to Items, recieves a pointer to Repo.
+
+// GetAll func: returns the value of Items, recieves a pointer to Repo.
