5dcbe473508507802ab6ef8664d896659c9b3251
Ibrahim Mohammedi <ibrahimod77@gmail.com>
2023-08-12 19:25:29 +0100

    filled the newsfeed.go

--- platform/newsfeed/newsfeed.go
+++ platform/newsfeed/newsfeed.go
@@ -1,11 +1,27 @@
 package newsfeed
 
 // Item sturcts : store the news feed items.
+type Item struct {
+	Title string `json:"title"`
+	Post  string `json :"post"`
+}
 
-// Repo struct : News feed list.
+// Repo struct : Creates a list of Items from Item.
+type Repo struct {
+	Items []Item
+}
 
 // New func: returns a pointer to Repo.
+func New() *Repo {
+	return &Repo{}
+}
 
 // Add func: Adds an item to Items, recieves a pointer to Repo.
+func (r *Repo) Add(item Item) {
+	r.Items = append(r.Items, item)
+}
 
 // GetAll func: returns the value of Items, recieves a pointer to Repo.
+func (r *Repo) GetAll() []Item {
+	return r.Items
+}
